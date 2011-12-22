3e0b6344ca81f5ff5fc82edbf523723b8cf6b8e6
Kirsten Jones <synedra@gmail.com>
2011-12-22 14:49:47 -0800

    Adding the reply url

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^reply', 'twilio_sms.views.sms_reply'),
 
     url(r'^login/?$', 'linkedin.views.oauth_login'),
     url(r'^logout/?$', 'linkedin.views.oauth_logout'),
